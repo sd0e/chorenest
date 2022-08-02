71a1cbe4c2a4ce04e68c20f2e23b9d504d8c1297
sebastiandoe5 <sebastiandoe5@gmail.com>
2022-08-02 13:19:49 +0100

    Add Firebase hosting code

--- src/firebase/addToAnalytics.js
+++ src/firebase/addToAnalytics.js
@@ -2,7 +2,7 @@ import { getAnalytics, logEvent } from 'firebase/analytics';
 
 const addToAnalytics = (title, path) => {
 	const analytics = getAnalytics();
-	logEvent(analytics, 'page_view', {'page_title': title, 'page_location': window.location.href, 'page_path': path});
+	logEvent(analytics, 'page_view', {page_title: title, page_location: window.location.href, page_path: path});
 }
 
 export default addToAnalytics;
