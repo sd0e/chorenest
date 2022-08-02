154d77a8ef951982a4a2d538fe7e4f993653cae0
sebastiandoe5 <sebastiandoe5@gmail.com>
2022-08-02 13:40:26 +0100

    Add icons to buttons and clarify account method

--- src/page/Account.js
+++ src/page/Account.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Button } from '@mui/material';
+import { createTheme, ThemeProvider, Button } from '@mui/material';
+import { DeleteForever, Logout } from '@mui/icons-material';
 
 import Header from '../Header';
 import deleteUserAccount from '../firebase/deleteUserAccount';
+import signOutUser from '../firebase/signOutUser';
 
 export default function Home() {
-	const deleteAccount = () => {
-		deleteUserAccount();
-	}
+	const theme = createTheme({
+		palette: {
+			mode: 'dark',
+		},
+	})
 
 	return (
 		<motion.div initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0.8 }} style={{ height: "100%" }}>
-			<Header Title="Account" />
-			<Button variant="outlined" color="error" onClick={deleteAccount}>Delete account</Button>
+			<ThemeProvider theme={theme}>
+				<Header Title="Account" />
+				<Button variant="outlined" color="warning" onClick={signOutUser} style={{ marginRight: '1rem' }}>
+					<Logout fontSize="small" style={{ marginRight: '0.5rem' }} />
+					Sign Out
+				</Button>
+				<Button variant="outlined" color="error" onClick={deleteUserAccount}>
+					<DeleteForever fontSize="small" style={{ marginRight: '0.5rem' }} />
+					Delete Account
+				</Button>
+			</ThemeProvider>
 		</motion.div>
 	)
 }
--- src/page/Base.js
+++ src/page/Base.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Button } from '@mui/material';
+import { createTheme, ThemeProvider, Button } from '@mui/material';
+import { Google, Login, PersonAdd } from '@mui/icons-material';
 
 import Header from '../Header';
 import classes from './Base.module.css';
@@ -8,17 +9,34 @@ import signInUser from '../firebase/signInUser';
 import config from '../config.json';
 
 export default function Base() {
-    const signIn = () => {
-        signInUser();
-    }
+    const theme = createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    });
 
 	return (
-		<motion.div initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0.8 }} className={classes.baseOuterContainer}>
-			<Header NoAuth />
-            <h2 className={classes.heading}>{config.name}</h2>
-            <span className={classes.subheading}>{config.short_description}</span>
-            <Button variant="outlined" color="primary" style={{ marginRight: '1rem' }} onClick={signIn}>Sign In</Button>
-            <Button variant="outlined" color="success" onClick={signIn}>Sign Up</Button>
+        <motion.div initial={{ opacity: 0, scale: 0.8 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0.8 }} className={classes.baseOuterContainer}>
+            <ThemeProvider theme={theme}>
+                <Header NoAuth />
+                <h2 className={classes.heading}>{config.name}</h2>
+                <span className={classes.subheading}>{config.short_description}</span>
+                <Button variant="outlined" color="primary" style={{ marginBottom: '2rem' }} onClick={signInUser}>
+                    <Google fontSize="small" style={{ marginRight: '0.5rem' }} />
+                    Continue with Google
+                </Button>
+
+                <br />
+
+                <Button variant="outlined" color="secondary" style={{ marginRight: '1rem' }} onClick={signInUser}>
+                    <Login fontSize="small" style={{ marginRight: '0.5rem' }} />
+                    Sign In
+                </Button>
+                <Button variant="outlined" color="success" onClick={signInUser}>
+                    <PersonAdd fontSize="small" style={{ marginRight: '0.5rem' }} />
+                    Sign Up
+                </Button>
+            </ThemeProvider>
 		</motion.div>
 	)
 }
